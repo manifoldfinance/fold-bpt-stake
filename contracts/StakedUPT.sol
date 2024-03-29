ab83dc8335ecdc81f2925246c26ec657fd3abccf
sandybradley <sandy.bradley@gmail.com>
2024-03-29 13:53:34 +0200

    chore: merge fix

--- contracts/StakedUPT.sol
+++ contracts/StakedUPT.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "./Owned.sol";
 import "./interfaces/IWETH.sol";
 
-import "@solmate/utils/ReentrancyGuard.sol";
+import "solmate/utils/ReentrancyGuard.sol";
 import "./interfaces/INonfungiblePositionManager.sol";
 
 /**
